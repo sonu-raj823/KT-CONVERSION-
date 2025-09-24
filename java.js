efe5203c804b097ee6dc2de45b3882d1c70aabb5
sonu-raj823 <sonu86036326421@gmail.com>
2025-09-24 07:07:38 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,308 @@
+// let fullName = "sonu"
+// let age = 20
+// let mobile = "vivo"
+
+
+
+// const sonu = {
+//     fullname : "sonu",
+//     age : 21,    
+// //     isPass : true,
+// //     useName : "sonubin",
+// // }
+// let a = 6
+// let b = 6
+// console.log("a + b = ", a + b);
+// console.log("a - b =", a - b);
+// console.log("a / b = " , a / b);
+// console.log('a * b =' , a * b)
+ 
+//     let age = 17;
+
+// if (age < 18) {
+//   console.log("you cannot vote");
+// }
+
+// if (age >= 18) {
+//   console.log("you can vote");
+// }
+
+// let age = 1;
+// if (age > 18){
+// console.log('you can vote');
+// }
+// if (age < 19){
+// console.log("you canote vote")
+// 
+// window.onload = function() {
+//     alert("I LOVE YOU JS");
+// };
+
+    // let mode = "dark"
+    // let anjali;
+
+    // if (mode == "dark"){
+    // anjali = "It's nighttime"
+    // }
+
+    // if (mode == "day"){
+    // anjali = "It's is dat time"
+    // };
+    // console.log(anjali);
+
+//prompt("Inter your marks");
+/*alert("I LOVE JS"); 
+// function greet(name){
+//     return "hello, " + name;}
+// console.log(greet("sonu")); */
+//console.log("sonu");
+
+
+/*-for(let i = 1 ; i <= 100 ; i++){
+    console.log("SONU")
+}
+console.log('loop has ended');
+
+for(let s = 1 ; s <=9999 ; s++ ){
+console.log(s+1)    
+} 
+let sum = 0;
+for (let s = 1 ; s <= 5 ; s++){
+    sum = sum+s;    
+}
+console.log(sum);
+console.log("loop has ended")
+
+let sonu = 0;
+let anjali = 555
+for(let i = 1 ; i <= anjali ; i++){
+    sonu = sonu+i;
+}
+console.log(sonu);
+for(let i = 1 ; i <= 5 ; i++){
+
+    console.log("i=", i)
+}
+let str = "JAVASCRIPET";
+let size = 0;
+for (let i of str){
+    console.log(i)
+    size++;
+}
+console.log("seiz =" ,  size)
+
+let student = {
+name: "sonu",
+age : 20,
+cgpa: 9.5,
+isPass:true,
+};
+for (let i in student){
+    console.log(i);
+}*/
+// for (let i = 1; i <= 100 ; i++){
+//     if(i%2 === 0)
+//     console.log("num = ", i)
+// }
+// for (let s = 1; s <= 100 ; s++){
+// if(s%2 ==! 0)
+//     console.log("num = ",s)
+
+// }
+
+// let sonu = [93,389,93,389,397,98,378,289];
+
+// for (let i = 0 ; i <= sonu.length ; i++){
+//     console.log(sonu[i]);
+// }
+
+
+
+// let jewelery = ["ring" , "pandan" , "nackless" , "breclet" , "earing" , "bangal"]
+ 
+// for ( let sonu of jewelery ){
+//     console.log(sonu);
+// }
+// for ( let i = 0 ; i < jewelery.length ; i++){
+//     console.log(jewelery[i].toUpperCase());
+// }
+// for (let sonu of jewelery) {
+// //     console.log(sonu);  
+
+// //}
+
+// let num = [45,98,2,56,45,80]
+// // console.log(num)
+// // for ( let i = 0 ; i < num.length ; ++i)
+// //     console.log(num[i]);
+
+// // for (let num1 of num) {
+// //     console.log(num1)\
+
+// //}
+
+// // let sum = 0  ;
+// // for (let i of num){
+// //  sum = sum + i;   
+// // }
+// // console.log(sum);
+
+
+// // let sum = 0;
+// // for (let i of num){
+// //     sum = i + sum;
+// // }
+// // let avg = sum/num.length; 
+// // console.log(avg)
+
+// // let sum = 0
+// // for (let i of num){
+// //     sum = i + sum;
+// // }
+// // console.log(sum)
+// // console.log(sum/num.length)
+
+
+// // let impute = Number(prompt());
+// // let impute1 = Number(prompt());
+// // sum = impute + impute1;
+// //  console.log(sum);
+
+
+// let sum = 0;
+
+// for (let  i of num ){
+//  sum = sum + i;
+//  avg = sum/num.length;
+// }
+// console.log(sum);
+// console.log(avg); 
+// let  num3 = Number(prompt("Inpute first name"));
+// let num2 = Number(prompt("inpute last name"));
+
+// console.log("sum" , num2 + num3);
+// console.log("avg", num2/num3 )
+
+// let JeweleryName = ["ring" , "nackless" , "brclect" , "pandan"]
+//     console.log(JeweleryName.toString());
+//     console.log(JeweleryName.length);
+
+// let sum = 10;
+
+// for (let i of num){
+//     console.log(i)
+// }
+
+// let sonu = ["Mouse" , "keybord" , "wire" , "androide Phone" , "phone"]
+// // sonu.push("pandrice" ," YearPhone")
+// // let sonu = [" HTML" , "CSS" , "JS"]
+// // let sonu1 = ["Pythne" , " ruby" , "Etc"]
+// // let sonu2 = sonu.concat(sonu1);
+
+// console.log(sonu.slice(1 , 4));
+
+// let sonu = ["ring" , "pendan" , "brclect"]
+// sonu.splice(0,1, "pandrive");
+
+// console.log(sonu);
+// let num =  [1,2,3,4,5,6,7,8]
+// num.splice(4,0,78,79);
+// let num = [1,2,3,4]
+// let num1 = [1,2,3,4]
+// num.shift(1)
+// console.log(num);
+// num1.pop(1);
+// console.log(num1)
+// let int = Number(prompt());
+// let int1 = Number(prompt());
+// console.log(int1 + int)
+// function sonu(a,b){
+//  let sum = a + b;
+//  let sdv = a - b;
+// console.log("sum =",sum);
+// console.log("sdv =", sdv);
+// console.log("mult = ", a * b);
+// console.log("div = ", a / b);
+// }
+// sonu(Number(prompt()),Number(prompt()));
+
+// function b(){
+
+// let a=document.getElementById("age").value;
+//  if(a < 18){
+//       console.log("you are not eligible to vote become you are minor")
+//  }
+//       else if(a >= 60) {
+//     console.log(" you are not eligible to you are ageded")
+// }      
+// else if(a >= 18){
+//     console.log("You are eligibal to vote")
+
+// }    else{
+//     console.log("you are eligibal but age")
+// }
+// }   
+
+function RecruitmentPg(){
+    let a=Number(document.getElementById("Recruitment").value);
+    let kt=Number(document.getElementById("kt").value);
+    let pg=Number(document.getElementById("pg").value);
+    let r= a/pg*kt;
+    let t= a-r
+    document.getElementById("result").innerHTML = 
+    "<br>"+
+    "Recruitment pg =" + r + "<br>" + "<br>" +
+    "Recruitment Alloy =" + (a-r) + "<br>" + "<br>"+
+    "Total Metal =" + (t+r);
+console.log("Recruitment PG = ", r);
+console.log("Recruitment Alloy", a-r);
+console.log("Total Metal",t+r);
+}
+    function pgkt(){
+    let pgValue=Number(document.getElementById("pg1").value);
+    let ktValue=Number(document.getElementById("kt1").value);
+    let ptValue=Number(document.getElementById("pt").value);
+    let Total=(pgValue*ptValue/ktValue);
+    document.getElementById("result1").innerHTML = 
+    "Total Metal =" + Total + "<br>" + "<br>" +
+    "Recruitment Alloy =" + (Total - pgValue);
+
+    console.log("total Metal =",Total)
+    console.log("Recruitment Alloy =", Total - pgValue);
+        
+}
+function lowkt(){
+let mtvalue=Number(document.getElementById("mw").value);
+let pkvalue=Number(document.getElementById("pk").value);
+let ckvalue=Number(document.getElementById("ck").value);
+let pt1vlue=Number(document.getElementById("pt1").value);
+
+let RecruitmentGold = mtvalue/pt1vlue*pkvalue;
+let a = (ckvalue/pt1vlue*mtvalue-RecruitmentGold)/(1-ckvalue/pt1vlue);
+let n = (mtvalue+a)
+document.getElementById("result3").innerHTML = 
+"Recruitment PG = " + a + "<br>" + "<br>" + 
+"Total Metal = " + n ;
+console.log("Recruitment PG = " ,  a);
+console.log("Total Metal = ", n)    
+}
+
+function hilow(){
+    let himtvalue=Number(document.getElementById("himt").value);
+    let hipkvalue=Number(document.getElementById("hipk").value);
+    let hickvalue=Number(document.getElementById("hick").value);
+    let hiypvalue=Number(document.getElementById("hiyp").value);
+        let mtpur=himtvalue / hiypvalue * hipkvalue;
+        let cnck=mtpur * hiypvalue / hickvalue;
+            let ra= cnck - himtvalue;
+            document.getElementById("hire").innerHTML = 
+            "Recruitment Alloy = " + ra + "<br>" + "<br>" +
+            "Total Metal = " + cnck + "<br>" + "<br>" +
+            "In Pure = " + mtpur;
+            console.log("Recruitment Alloy = ", ra )
+            console.log("Total Metal = ", cnck)
+            console.log("in Pure = ",  mtpur)
+
+
+
+}
